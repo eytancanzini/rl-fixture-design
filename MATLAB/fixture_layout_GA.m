b247069dd7bc8374e312b1ec3c9c43dc686db446
Ethan Canzini <ethan.canzini@gmail.com>
2022-04-05 10:51:48 +0100

    Added GA and RL files

---
+++ MATLAB/fixture_layout_GA.m
@@ -0,0 +1,3 @@
+%% Genetic Algorithm Optimisation for Fixture Location
+% This file will have the creation of a genetic algorithm that is used to
+% determine where to place the fixtures. 
---
+++ MATLAB/fixture_layout_RL.m
@@ -0,0 +1,5 @@
+%% Reinforcement Learning for Fixture Layout Planning
+% This file will introduce reinforcement learning (RL) as a method of
+% generating optimal fixture plans for a component. This approach treats
+% the fixture planning as a control problem rather than a dynamic
+% programming problem.
