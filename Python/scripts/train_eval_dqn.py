d50aef80d559087d24f5dd20bbfe1bd0fb16cd0a
Eytan Canzini <ethan.canzini@gmail.com>
2022-06-12 22:29:50 +0100

    Added visualisation for default test

--- Python/scripts/train_eval_dqn.py
+++ Python/scripts/train_eval_dqn.py
@@ -22,11 +22,7 @@ from tf_agents.environments import tf_py_environment
 from tf_agents.networks import q_network
 from tf_agents.utils import common
 
-
-flags.DEFINE_string('root_dir', os.getenv('TEST_UNDECLARED_OUTPUTS_DIR'),
-                    'Root directory for writing logs/summaries/checkpoints.')
-
-FLAGS = flags.FLAGS
+ROOT_DIR = '../checkpoints/'
 
 BATCH_SIZE = 8
 CONTEXT_DIM = 15
@@ -37,56 +33,56 @@ STEPS_PER_LOOP = 2  # DQN agent requires time dim=2.
 
 
 def main():
-  tf.compat.v1.enable_v2_behavior()  # The trainer only runs with V2 enabled.
+    tf.compat.v1.enable_v2_behavior()  # The trainer only runs with V2 enabled.
 
-  with tf.device('/CPU:0'):  # due to b/128333994
-    action_reward_fns = (
-        environment_utilities.sliding_linear_reward_fn_generator(
-            CONTEXT_DIM, NUM_ACTIONS, REWARD_NOISE_VARIANCE))
+    with tf.device('/CPU:0'):  # due to b/128333994
+        action_reward_fns = (
+            environment_utilities.sliding_linear_reward_fn_generator(
+                CONTEXT_DIM, NUM_ACTIONS, REWARD_NOISE_VARIANCE))
 
-    env = sspe.StationaryStochasticPyEnvironment(
-        functools.partial(
-            environment_utilities.context_sampling_fn,
-            batch_size=BATCH_SIZE,
-            context_dim=CONTEXT_DIM),
-        action_reward_fns,
-        batch_size=BATCH_SIZE)
-    environment = tf_py_environment.TFPyEnvironment(env)
+        env = sspe.StationaryStochasticPyEnvironment(
+            functools.partial(
+                environment_utilities.context_sampling_fn,
+                batch_size=BATCH_SIZE,
+                context_dim=CONTEXT_DIM),
+            action_reward_fns,
+            batch_size=BATCH_SIZE)
+        environment = tf_py_environment.TFPyEnvironment(env)
 
-    optimal_reward_fn = functools.partial(
-        environment_utilities.tf_compute_optimal_reward,
-        per_action_reward_fns=action_reward_fns)
+        optimal_reward_fn = functools.partial(
+            environment_utilities.tf_compute_optimal_reward,
+            per_action_reward_fns=action_reward_fns)
 
-    optimal_action_fn = functools.partial(
-        environment_utilities.tf_compute_optimal_action,
-        per_action_reward_fns=action_reward_fns)
+        optimal_action_fn = functools.partial(
+            environment_utilities.tf_compute_optimal_action,
+            per_action_reward_fns=action_reward_fns)
 
-    q_net = q_network.QNetwork(
-        environment.observation_spec(),
-        environment.action_spec(),
-        fc_layer_params=(50, 50))
+        q_net = q_network.QNetwork(
+            environment.observation_spec(),
+            environment.action_spec(),
+            fc_layer_params=(50, 50))
 
-    agent = dqn_agent.DqnAgent(
-        environment.time_step_spec(),
-        environment.action_spec(),
-        q_network=q_net,
-        epsilon_greedy=0.1,
-        target_update_tau=0.05,
-        target_update_period=5,
-        optimizer=tf.compat.v1.train.AdamOptimizer(learning_rate=1e-2),
-        td_errors_loss_fn=common.element_wise_squared_loss)
+        agent = dqn_agent.DqnAgent(
+            environment.time_step_spec(),
+            environment.action_spec(),
+            q_network=q_net,
+            epsilon_greedy=0.1,
+            target_update_tau=0.05,
+            target_update_period=5,
+            optimizer=tf.compat.v1.train.AdamOptimizer(learning_rate=1e-2),
+            td_errors_loss_fn=common.element_wise_squared_loss)
 
-    regret_metric = tf_bandit_metrics.RegretMetric(optimal_reward_fn)
-    suboptimal_arms_metric = tf_bandit_metrics.SuboptimalArmsMetric(
-        optimal_action_fn)
+        regret_metric = tf_bandit_metrics.RegretMetric(optimal_reward_fn)
+        suboptimal_arms_metric = tf_bandit_metrics.SuboptimalArmsMetric(
+            optimal_action_fn)
 
-    trainer.train(
-        root_dir=FLAGS.root_dir,
-        agent=agent,
-        environment=environment,
-        training_loops=TRAINING_LOOPS,
-        steps_per_loop=STEPS_PER_LOOP,
-        additional_metrics=[regret_metric, suboptimal_arms_metric])
+        trainer.train(
+            root_dir=ROOT_DIR,
+            agent=agent,
+            environment=environment,
+            training_loops=TRAINING_LOOPS,
+            steps_per_loop=STEPS_PER_LOOP,
+            additional_metrics=[regret_metric, suboptimal_arms_metric])
 
 
 if __name__ == '__main__':
